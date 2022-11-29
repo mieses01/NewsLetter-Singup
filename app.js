9dea956ab316c800e2593c4d91abf6c10b8d1469
mmieses01 <manuel.mieses48@gmail.com>
2022-11-28 17:04:56 -0800

    completo con post muestra datos en console

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const app = express();
 
 
 //codigo
+// Static Files
 app.use(express.static("static"));
-
+//body prase
+app.use(bodyparser.urlencoded({extended:true}));
+//get with template
 app.get("/",function(req, res){
   res.sendFile(__dirname+"/singup.html");
-})
+});
 
+app.post("/",function(req, res){
+  var firstname = req.body.fname;
+  var lastname = req.body.lname;
+  var mail = req.body.email;
+  console.log(firstname,lastname,mail);
+});
+//app listen
 app.listen(3000,function(){
   console.log("Running in port 3000");
-})
+});
